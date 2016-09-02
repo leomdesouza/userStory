80cf639d124255cc7fbd09784890511b72b384cf
leomdesouza <leosouza@kiddolabs.com>
2016-09-01 21:05:23 -0300

    video 5

--- server.js
+++ server.js
@@ -4,6 +4,14 @@ var morgan = require('morgan');
 
 var app =  express();
 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(morgan('dev'));
+
+app.get('*', function(req, res){
+    res.sendFile(__dirname + '/public/views/index.html');
+});
+
 app.listen(3000, function(err){
     if(err){
         console.log(err);
