7b2a04af586ef9ade4e8979cb0ec156bcc24a0ad
leomdesouza <leosouza@kiddolabs.com>
2016-09-16 00:19:29 -0300

    Angular setup

--- server.js
+++ server.js
@@ -24,10 +24,12 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(morgan('dev'));
 
+app.use(express.static(__dirname + '/public'));
+
 app.use('/api',api);
 
 app.get('*', function(req, res){
-    res.sendFile(__dirname + '/public/views/index.html');
+    res.sendFile(__dirname + '/public/app/views/index.html');
 });
 
 app.listen(config.port, function(err){
